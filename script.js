bb355a5df5ee4559f585d9934cbce420a4507944
DBENTLEY <david.bentley95@gmail.com>
2024-12-09 06:24:04 -0800

    Add JS validation control for password fields
    Ensure that if the passwords do not match the fields are
    red until they do match. Toggle message indicating the fields
    must match until they do.

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
+const passwordInput = document.querySelector("#password");
+const confirmPasswordInput = document.querySelector("#confirm-password");
+const passwordErrorMsg = document.querySelector(".password-error");
 
+function checkPassword(event) {
+
+    if(event.target.value !== passwordInput.value) {
+        event.target.style.borderColor = "#C62E2E";
+        passwordInput.style.borderColor = "#C62E2E";
+        passwordErrorMsg.style.display = "block";
+        passwordErrorMsg.style.color = "#C62E2E";
+    }
+    if(event.target.value === passwordInput.value) {
+        event.target.style.borderColor = "#8EB486";
+        passwordInput.style.borderColor = "#8EB486";
+        passwordErrorMsg.style.display = "none";
+    }
+}
+
+passwordInput.addEventListener("input", checkPassword);
+confirmPasswordInput.addEventListener("input", checkPassword);
 
