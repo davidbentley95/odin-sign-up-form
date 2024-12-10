ee003016e5e3b2361c561bc9d1b74ea168f46cef
DBENTLEY <david.bentley95@gmail.com>
2024-12-09 20:16:45 -0800

    Patch password validation bug
    Validation error would clear when entering input to the password
    field after triggering the error validation in the confirm password
    field.

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const phoneNumberInput = document.querySelector("#phone-number");
 
 function checkPassword(event) {
 
-    if(event.target.value !== passwordInput.value) {
+    if(event.target.value !== passwordInput.value || event.target.value !== confirmPasswordInput.value) {
         event.target.style.borderColor = "#C62E2E";
         passwordInput.style.borderColor = "#C62E2E";
         passwordErrorMsg.style.display = "block";
         passwordErrorMsg.style.color = "#C62E2E";
     }
-    if(event.target.value === passwordInput.value) {
+    if(event.target.value === passwordInput.value && event.target.value === confirmPasswordInput.value) {
         event.target.style.borderColor = "#8EB486";
         passwordInput.style.borderColor = "#8EB486";
         passwordErrorMsg.style.display = "none";
